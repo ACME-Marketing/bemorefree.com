7b7a8b3eac6544e8556dfa86a6db8f0ab9781c6f
saltlifeshep <62799544+saltlifeshep@users.noreply.github.com>
2025-07-26 16:43:45 -0400

    Fixed cal.com links

---
+++ src/config/booking.ts
@@ -0,0 +1,21 @@
+export const BOOKING_CONFIG = {
+  cal: {
+    username: "takealook",
+    events: {
+      freeSession: {
+        slug: "neutral-zone-free-session",
+        namespace: "neutral-zone-free-session",
+        config: {
+          layout: "month_view"
+        }
+      },
+      paidSession: {
+        slug: "neutral-zone-one-hour-session",
+        namespace: "neutral-zone-one-hour-session",
+        config: {
+          layout: "month_view"
+        }
+      }
+    }
+  }
+};
